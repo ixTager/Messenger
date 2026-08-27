552bc975c0444f66546db08b3d97cf2dbfb92871
Extager <164411975+Extager@users.noreply.github.com>
2026-08-27 22:46:20 +0300

    render messages in dialog

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,18 +1,3 @@
-const dialogId = window.location.pathname.split("/").pop();
-
-const loadMessages = async () => {
-
-    const res = await fetch(`/api/chats/${dialogId}`);
-
-    if (!res.ok) {
-        throw new Error("Cannot load messages: " + res.status);
-    }
-
-    const messages = await res.json();
-
-    messages.forEach(message => renderNewMsg(message));
-};
-
 const openChat = async () => {
     try {
         await loadMessages();
